b3919080335f3fd8f75b8408fda1e10a32ea62f7
Christian Tverberg <christian.tverberg@tietoevry.com>
2024-08-20 09:02:37 +0200

    Add basic DB setup for Trends and transactions

--- main.go
+++ main.go
@@ -7,18 +7,46 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 
 	"cloud.google.com/go/datastore"
 )
 
 type Trend struct {
-	Message string `datastore:"message"`
+	Stock       string      `datastore:"stock"`
+	TrendScore  float64     `datastore:"trend_score"`
+	Date        time.Time   `datastore:"date"`
+	TrendValues TrendValues `datastore:"trend_values"`
 }
 
 type TrendResponse struct {
-	ID      int64  `json:"id"`
-	Message string `json:"message"`
+	ID int64 `json:"id"`
+	Trend
 }
+
+type Transaction struct {
+	TrendID     *datastore.Key `datastore:"trend_id"`
+	DateBought  time.Time      `datastore:"date_bought"`
+	DateSold    time.Time      `datastore:"date_sold"`
+	PriceBought float64        `datastore:"price_bought"`
+	PriceSold   float64        `datastore:"price_sold"`
+	Volume      int64          `datastore:"volume"`
+}
+
+type TrendValues struct {
+	IndicatorName  string        `json:"indicator_name"`
+	IndicatorType  IndicatorType `json:"indicator_type"`
+	IndicatorValue float64       `json:"indiator_value"`
+}
+
+type IndicatorType int64
+
+const (
+	IndicatorOver      IndicatorType = 1
+	IndicatorUnder     IndicatorType = 2
+	IndicatorCrossUp   IndicatorType = 3
+	IndicatorCrossDown IndicatorType = 4
+)
 
 func main() {
 	http.HandleFunc("/", helpHandler)
@@ -58,10 +86,9 @@ func getTrendsHandler(w http.ResponseWriter, r *http.Request) {
 	// Prepare a slice of TrendResponse to return, including the numeric ID
 	var response []TrendResponse
 	for i, key := range keys {
-
 		response = append(response, TrendResponse{
-			ID:      key.ID,
-			Message: trends[i].Message,
+			ID:    key.ID,
+			Trend: trends[i],
 		})
 	}
 
@@ -85,11 +112,27 @@ func saveHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	// Parse the request body
-	var trend Trend
-	err := json.NewDecoder(r.Body).Decode(&trend)
+	var requestData struct {
+		Stock string `json:"stock"`
+	}
+	err := json.NewDecoder(r.Body).Decode(&requestData)
 	if err != nil {
 		http.Error(w, "Bad request", http.StatusBadRequest)
 		return
+	}
+
+	// Fetch data here...
+	//trendScore, trendValues, transactions, err := fetchTrendData()
+	trendValues := TrendValues{}
+	trendValues.IndicatorName = "RSI"
+	trendValues.IndicatorType = IndicatorOver
+	trendValues.IndicatorValue = 70
+
+	trend := Trend{
+		Stock:       requestData.Stock,
+		Date:        time.Now(),
+		TrendScore:  56.4,
+		TrendValues: trendValues,
 	}
 
 	// Set up Datastore client
@@ -100,15 +143,53 @@ func saveHandler(w http.ResponseWriter, r *http.Request) {
 	}
 	defer client.Close()
 
-	if err := saveTrend(ctx, client, &trend); err != nil {
+	// Save trend results:
+	trendKey := datastore.IncompleteKey("Trend", nil)
+	completeKey, err := client.Put(ctx, trendKey, &trend)
+	if err != nil {
 		log.Printf("Failed to save trend: %v", err)
 		http.Error(w, "Failed to save trend", http.StatusInternalServerError)
 		return
 	}
 
+	// Save transactions for Trend:
+	transactions := []Transaction{
+		{
+			DateBought:  time.Now(),
+			DateSold:    time.Now().Add(1 * time.Hour),
+			PriceBought: 150,
+			PriceSold:   156,
+			Volume:      100,
+		},
+		{
+			DateBought:  time.Now().Add(1*time.Hour + 30*time.Minute),
+			DateSold:    time.Now().Add(2 * time.Hour),
+			PriceBought: 154,
+			PriceSold:   159,
+			Volume:      100,
+		},
+		{
+			DateBought:  time.Now().Add(2*time.Hour + 30*time.Minute),
+			DateSold:    time.Now().Add(3 * time.Hour),
+			PriceBought: 160,
+			PriceSold:   163,
+			Volume:      100,
+		},
+	}
+	for _, transaction := range transactions {
+		transaction.TrendID = completeKey
+		transactionKey := datastore.IncompleteKey("Transaction", nil)
+		_, err := client.Put(ctx, transactionKey, &transaction)
+		if err != nil {
+			log.Printf("Failed to save transaction: %v", err)
+			http.Error(w, "Failed to save transaction", http.StatusInternalServerError)
+			return
+		}
+	}
+
 	// Response
 	w.WriteHeader(http.StatusCreated)
-	fmt.Fprintf(w, "Trend saved successfully")
+	fmt.Fprintf(w, "Trends saved successfully")
 }
 
 func saveTrend(ctx context.Context, client *datastore.Client, trend *Trend) error {
@@ -122,4 +203,10 @@ func saveTrend(ctx context.Context, client *datastore.Client, trend *Trend) erro
 	}
 
 	return nil
+}
+
+func saveTransaction(ctx context.Context, client *datastore.Client, transaction *Transaction) error {
+	key := datastore.IncompleteKey("Transaction", nil)
+	_, err := client.Put(ctx, key, transaction)
+	return err
 }
